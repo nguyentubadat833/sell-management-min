7c9162bb96121c76ecd21a77aebe2009b83a4f7a
nguyentubadat <nguyentubadat833@gmail.com>
2024-12-12 15:53:35 +0700

    create prisma schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@prisma/nuxt"],
+
+  prisma: {
+    installStudio: false,
+    installCLI: false,
+    installClient: false,
+  },
+
   css: ['~/assets/css/main.css'],
 
   future: {
